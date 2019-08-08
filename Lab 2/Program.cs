e3c1363cfec805eaf5c3e343de0847c55fd547aa
gmullins925 <50452654+gmullins925@users.noreply.github.com>
2019-08-08 11:04:52 -0500

    Commit

---
+++ Lab 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+
+	/*
+Project:    Lab 2 Calculate Batting Average
+Date:        to/da/y
+Author:     You the big new programmer
+Notes:      This program reads in a baseball player's name,
+            number of hits and number of at bats.
+            The player's batting average is calculated and displayed
+			*/
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
